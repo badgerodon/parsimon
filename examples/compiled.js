7c71f45b52bf75fbaec26f06d8f9737fb87946f3
Caleb Doxsey <cdoxsey@trada.com>
2012-03-19 10:01:13 -0600

    Latest

--- examples/compiled.js
+++ examples/compiled.js
@@ -6,18 +6,118 @@ function pp() {
   }
 }
 
-Interpreter = (function() {
-  S = 1;
-  R = 1;
-  SYMBOLS = {};
-  function to_sym(str) {
-    if (!SYMBOLS[str]) {
-      SYMBOLS[str] = S++;
-    }
-    return SYMBOLS[str];
-  }
+Interpreter = {
+  symbols: {},
+  symbol_count: 0,
+  kind_to_record: {},
   
-  function binary_search(fields, symbol) {
+  to_symbol: function(str) {
+    if (!this.symbols[str]) {
+      this.symbols[str] = ++this.symbol_count;
+    }
+    return this.symbols[str];
+  },
+ 
+  mk_error: function(msg) {      
+    return {
+      kind: this.to_symbol("Error"),
+      value: msg,
+      fields: []
+    };
+  },
+  mk_function: function(f) {
+    return {
+      kind: this.to_symbol("Function"),
+      value: f,
+      fields: []
+    };
+  },
+  mk_global_scope: function() {
+    var scope = this.mk_scope();
+    
+    var types = [
+      "Error",
+      "Record",
+      "Module",
+      
+      "Boolean",
+      "Integer",
+      "Float",
+      "Reference",
+      "Function",
+      "NativeFunction"
+    ];
+    for (var i=0; i<types.length; i++) {
+      var rec = this.mk_record();
+      this.kind_to_record[this.to_symbol(types[i])] = rec;
+      this.set(scope, this.to_symbol(types[i]), rec);
+    }
+    
+    var c = this.mk_module();
+    this.set(c, this.to_symbol("log"), this.mk_native_function(function() {
+      //pp(arguments);
+      return this.mk_null();
+    }));
+    this.set(scope, this.to_symbol("Console"), c);
+    
+    return scope;
+  },
+  mk_module: function() {
+    return {
+      kind: this.to_symbol("Module"),
+      value: null,
+      fields: []
+    }
+  },
+  mk_native_function: function(f) {
+    return {
+      kind: this.to_symbol("NativeFunction"),
+      value: f,
+      fields: []
+    };      
+  },
+  mk_null: function() {      
+    return {
+      kind: this.to_symbol("Null"),
+      value: null,
+      fields: []
+    };
+  },
+  mk_record: function() {      
+    var r = {
+      kind: this.to_symbol("Record"),
+      value: {
+        id: this.symbol_count++,
+        parameters: []
+      },
+      fields: []
+    };
+    this.kind_to_record[r.value.id] = r;
+    return r;
+  },
+  mk_reference: function(value) {      
+    return {
+      kind: this.to_symbol("Reference"),
+      value: value,
+      fields: []
+    };
+  },
+  mk_scope: function(parent) {
+    return {
+      kind: this.to_symbol("Scope"),
+      value: parent || null,
+      fields: []
+    };      
+  },
+  mk_string: function(str) {      
+    return {
+      kind: this.to_symbol("String"),
+      value: str,
+      fields: []
+    };
+  },
+  
+  binary_search: function(fields, symbol) {
     var imin=0,
       imax=fields.length-1,
       imid;
@@ -37,10 +137,18 @@ Interpreter = (function() {
     }
     
     return -1;
-  }
-  
-  function set(parent, symbol, child) {
-    var pos = binary_search(parent.fields, symbol);
+  },
+  get: function(parent, symbol) {
+    var pos = this.binary_search(parent.fields, symbol);
+    
+    if (pos == -1) {
+      return this.mk_error("Unknown Field");
+    } else {
+      return parent.fields[pos];
+    }      
+  },
+  set: function(parent, symbol, child) {
+    var pos = this.binary_search(parent.fields, symbol);
     
     // Create the field
     var field = {
@@ -68,279 +176,201 @@ Interpreter = (function() {
     // Replace existing
     } else {
       parent.fields[pos] = field;
-    }
-  }
-  
-  function get(parent, symbol, result) {
-    var pos = binary_search(parent.fields, symbol);
-    
-    if (pos == -1) {
-      result.kind = to_sym("Error");
-      result.value = {
-        message: "Unknown Field"
-      };
-      result.fields = [];
-    } else {
-      result.kind = parent.fields[pos].kind;
-      result.value = parent.fields[pos].value;
-      result.fields = parent.fields[pos].fields;
-    }
-  }
-  
-  function invoke(scope, value, args, result) {
-    if (value.kind == to_sym("Function")) {
+    }      
+  },
+  invoke: function(scope, value, args) {
+    if (value.kind == this.to_symbol("Function")) {
       var f = value.value;
       if (f.parameters.length != args.length) {
-        result.kind = to_sym("Error");
-        result.value = {
-          message: "Wrong # of arguments passed to function"
-        };
-        result.fields = [];
-        return;
+        return mk_error("Wrong # of arguments passsed to function");
       }
       
-      var s = mk_scope(scope);
+      var s = this.mk_scope(scope);
       for (var i=0; i<f.parameters.length; i++) {
-        set(s, f.parameters[i], args[i]);
+        this.set(s, f.parameters[i], args[i]);
       }
-      var r = Interpreter.execute(scope, f.block);
-      result.kind = r.kind;
-      result.value = r.value;
-      result.fields = r.fields;
-    } else if (value.kind == to_sym("NativeFunction")) {
-      value.value(args, result);
+      return this.execute(s, f.block);
+    } else if (value.kind == this.to_symbol("NativeFunction")) {
+      return value.value.apply(this, args);
     } else {
-      result.kind = to_sym("Error");
-      result.value = {
-        message: "Non-invokable object"
-      };
-      result.fields = [];
+      return this.mk_error("Non-invokable object");
     }
-  }
+  },
   
-  function mk_scope(parent) {
-    return {
-      kind: to_sym("Scope"),
-      value: parent,
-      fields: []
-    };
-  }
-  function mk_module() {
-    return {
-      kind: to_sym("Module"),
-      value: null,
-      fields: []
-    };
-  }
-  function mk_record() {
-    return {
-      kind: to_sym("Record"),
-      value: R++,
-      fields: []
-    };
-  }
-  function mk_native_function(f) {
-    return {
-      kind: to_sym("NativeFunction"),
-      value: f,
-      fields: []
-    };
-  }
-  function mk_function(f) {
-    return {
-      kind: to_sym("Function"),
-      value: f,
-      fields: []
-    };
-  }
-  function mk_string(str) {
-    return {
-      kind: to_sym("String"),
-      value: str,
-      fields: []
-    };
-  }
-  function mk_null() {
-    return {
-      kind: to_sym("Null"),
-      value: null,
-      fields: []
-    };
-  }
-  function mk_reference(value) {
-    return {
-      kind: to_sym("Reference"),
-      value: value,
-      fields: []
-    };
-  }
-  function mk_error(msg) {
-    return {
-      kind: to_sym("Error"),
-      value: msg,
-      fields: []
-    };
-  }
-  
-  function native_error_to_string(args, result) {
-    result.kind = to_sym("String");
-    result.value = args[0].value.message;
-  }
-  function native_integer_to_string(args, result) {
-    var r = mk_string(args[0].value.toString());
-    result.kind = r.kind;
-    result.value = r.value;
-    result.fields = r.fields;
-  }
-  function native_console_log(args, result) {
-    console.log("Console.log", args);
-    result.kind = to_sym("Null");
-    result.value = null;
-    result.fields = [];
-  }
-  
-  function get_global_scope() {
-    var global = mk_scope(null);
+  execute: function(scope, block) {
+    if (!scope) {
+      scope = this.mk_global_scope();
+    }
     
-    var error = mk_record("Error");
-    set(error, to_sym("to_string"), mk_native_function(native_error_to_string));
-    set(global, to_sym("Error"), error);
-    
-    var integer = mk_record("Integer");
-    set(integer, to_sym("to_string"), mk_native_function(native_integer_to_string));
-    set(global, to_sym("Integer"), integer);
-    
-    var c = mk_module();
-    set(c, to_sym("log"), mk_native_function(native_console_log));
-    set(global, to_sym("Console"), c);
-    
-    return global;    
-  }
-  
-  return {
-    execute: function(scope, block) {
-      if (!scope) {
-        scope = get_global_scope();
+    var e = this.mk_null();
+    for (var i=0; i<block.expressions.length; i++) {
+      var exp = block.expressions[i];
+      e = this.execute_expression(scope, exp);
+      if (e.kind == this.to_symbol("Error") || exp.type == 'return') {
+        break;
       }
-      
-      var e = mk_null();
-      for (var i=0; i<block.expressions.length; i++) {
-        e = this.execute_expression(scope, block.expressions[i]);
-        if (e.kind == to_sym("Error")) {
-          break;
-        }
-      }
-      
-      //pp(SYMBOLS, scope, e);
+    }
+    
+    return e;
+  },
+  execute_expression: function(scope, exp) {
+    switch(exp.type) {
+    case 'address':
+      return this.execute_address(scope, exp);
+    case 'assignment':
+      return this.execute_assignment(scope, exp.left, exp.right);
+    case 'constructor':
+      return this.execute_constructor(scope, exp);
+    case 'dereference':
+      return this.execute_dereference(scope, exp);
+    case 'function_definition':
+      return this.execute_function_definition(scope, exp);
+    case 'identifier':
+      return this.execute_identifier(scope, exp);
+    case 'invocation':
+      return this.execute_invocation(scope, exp);
+    case 'member':
+      return this.execute_member(scope, exp);
+    case 'null':
+      return this.execute_null(scope, exp);
+    case 'numeric_literal':
+      return this.execute_numeric_literal(scope, exp);
+    case 'record_definition':
+      return this.execute_record_definition(scope, exp);
+    case 'return':
+      return this.execute_return(scope, exp);
+    case 'typeof':
+      return this.execute_typeof(scope, exp);
+    }
+    pp("UNKNOWN EXPRESSION: ", exp);
+    return null;
+  },
+  execute_address: function(scope, exp) {
+    var e = this.execute_expression(scope, exp.expression);
+    if (e.kind == this.to_symbol("Error")) {
       return e;
-    },
-    execute_expression: function(scope, exp) {
-      switch(exp.type) {
-      case 'address':
-        return this.execute_address(scope, exp);
-      case 'assignment':
-        return this.execute_assignment(scope, exp.left, exp.right);
-      case 'constructor':
-        return this.execute_constructor(scope, exp);
-      case 'function_definition':
-        return this.execute_function_definition(scope, exp);
-      case 'identifier':
-        return this.execute_identifier(scope, exp);
-      case 'invocation':
-        return this.execute_invocation(scope, exp);
-      case 'member':
-        return this.execute_member(scope, exp);
-      case 'record_definition':
-        return this.execute_record_definition(scope, exp);
-      case 'return':
-        return this.execute_return(scope, exp);
-      }
-      console.error("UNKNOWN EXPRESSION: ", exp);
-      return null;
-    },
-    execute_address: function(scope, exp) {
-      var e = this.execute_expression(exp);
-      if (e.type == to_sym("Error")) {
-        return e;
-      }
-      return mk_reference(e);
-    },
-    execute_assignment: function(scope, left, right) {
-      var root = scope;
-      if (left.type != 'identifier') {
-        root = this.execute_expression(scope, left.left);
-        left = left.right;
-      }
-      var result = this.execute_expression(scope, right);
-      if (result.kind == to_sym("Error")) {
-        return result;
-      }
-      set(root, to_sym(left.value), result);
-      return result;
-    },
-    execute_constructor: function(scope, exp) {      
-      var e = this.execute_expression(scope, exp.record);
-      if (e.kind == to_sym("Error")) {
-        return e;
-      }
-      if (e.kind != to_sym("Record")) {
-        return mk_error("Can only construct a record using a record type");
-      }
-      if (e.fields.length != exp.arguments.length) {
-        return mk_error("Wrong # of arguments passed to constructor");
-      }
-      var r = {
-        kind: 
-      }
-      for (var i=0; i<e.fields.length; i++) {
-        var a = this.execute_expression(scope, e.arguments[i]);
-        if (a.kind == to_sym("Error")) {
-          return a;
-        }
-        
-      }
-    },
-    execute_function_definition: function(scope, exp) {
-      return mk_function(exp);
-    },
-    execute_identifier: function(scope, exp) {
-      var r = {};
-      get(scope, to_sym(exp.value), r);
-      return r;
-    },
-    execute_invocation: function(scope, exp) {
-      var e = this.execute_expression(scope, exp.expression);
-      var args = [];
-      for (var i=0; i<exp.arguments.length; i++) {
-        var a = this.execute_expression(scope, exp.arguments[i]);
-        if (a.kind == to_sym("Error")) {
-          return a;
-        }
-        args.push(a);
-      }
-      var r = {};
-      invoke(scope, e, args, r);
-      return r;
-    },
-    execute_member: function(scope, exp) {
-      var r = {};
-      var left = this.execute_expression(scope, exp.left);
-      get(left, to_sym(exp.right.value), r);
-      return r;
-    },
-    execute_record_definition: function(scope, record) {
-      var r = mk_record();
-      r.value = [];
-      for (var i=0; i<record.fields.length; i++) {
-        r.value.push(to_sym(record.fields[i].name));
-      }
-      return r;
     }
-  };
-})();
+    return this.mk_reference(e);
+  },
+  execute_assignment: function(scope, left, right) {
+    var root = scope;
+    if (left.type != 'identifier') {
+      root = this.execute_expression(scope, left.left);
+      left = left.right;
+    }
+    if (root.kind === this.to_symbol("Error")) {
+      return root;
+    }
+    var result = this.execute_expression(scope, right);
+    if (result.kind == this.to_symbol("Error")) {
+      return result;
+    }
+    this.set(root, this.to_symbol(left.value), result);
+    return result;
+  },
+  execute_constructor: function(scope, exp) {      
+    var e = this.execute_expression(scope, exp.expression);
+    if (e.kind == this.to_symbol("Error")) {
+      return e;
+    }
+    if (e.kind != this.to_symbol("Record")) {
+      return mk_error("Can only construct a record using a record type");
+    }
+    var rec = e.value;
+    if (rec.parameters.length != exp.arguments.length) {
+      return mk_error("Wrong # of arguments passed to constructor");
+    }
+    var r = {
+      kind: rec.id,
+      value: null,
+      fields: []
+    };
+    for (var i=0; i<rec.parameters.length; i++) {
+      var a = this.execute_expression(scope, exp.arguments[i]);
+      if (a.kind == this.to_symbol("Error")) {
+        return a;
+      }
+      r.fields.push({
+        symbol: rec.parameters[i],
+        kind: a.kind,
+        value: a.value,
+        fields: a.fields
+      });
+    }
+    return r;
+  },
+  execute_dereference: function(scope, exp) {
+    var e = this.execute_expression(scope, exp.expression);
+    if (e.kind == this.to_symbol("Error")) {
+      return e;
+    }
+    if (e.kind !== this.to_symbol("Reference")) {
+      return this.mk_error("Can only dereference a reference type");
+    }
+    return e.value;
+  },
+  execute_function_definition: function(scope, exp) {
+    return this.mk_function(exp);
+  },
+  execute_identifier: function(scope, exp) {
+    return this.get(scope, this.to_symbol(exp.value));
+  },
+  execute_invocation: function(scope, exp) {
+    var e = this.execute_expression(scope, exp.expression);
+    var args = [];
+    for (var i=0; i<exp.arguments.length; i++) {
+      var a = this.execute_expression(scope, exp.arguments[i]);
+      if (a.kind == this.to_symbol("Error")) {
+        return a;
+      }
+      args.push(a);
+    }
+    return this.invoke(scope, e, args);
+  },
+  execute_member: function(scope, exp) {
+    var r = {};
+    var left = this.execute_expression(scope, exp.left);
+    return this.get(left, this.to_symbol(exp.right.value));
+  },
+  execute_null: function(scope, exp) {
+    return {
+      kind: this.to_symbol("Null"),
+      value: null,
+      fields: []
+    };
+  },
+  execute_numeric_literal: function(scope, exp) {
+    return {
+      kind: this.to_symbol("Integer"),
+      value: exp.value,
+      fields: []
+    };
+  },
+  execute_record_definition: function(scope, record) {
+    var r = this.mk_record();
+    for (var i=0; i<record.fields.length; i++) {
+      r.value.parameters.push(this.to_symbol(record.fields[i].name));
+    }
+    return r;
+  },
+  execute_return: function(scope, exp) {
+    var e = this.execute_expression(scope, exp.expression);
+    return e;
+  },
+  execute_typeof: function(scope, exp) {
+    var e = this.execute_expression(scope, exp.expression);
+    if (e.kind == this.to_symbol("Error")) {
+      return e;
+    }
+    if (!this.kind_to_record[e.kind]) {
+      return this.mk_error("Unknown record type " + e.kind);
+    }
+    return this.kind_to_record[e.kind];
+  }
+};
 
 function main() {
-  Interpreter.execute(null, {
+  pp("RESULT", Interpreter.execute(null, {
     type: 'block',
     expressions: [{
       type: 'assignment',
@@ -394,7 +424,7 @@ function main() {
               type: 'address',
               expression: {
                 type: 'constructor',
-                record: {
+                expression: {
                   type: 'identifier',
                   value: 'Tree'
                 },
@@ -442,11 +472,17 @@ function main() {
         }
       },
       arguments: [{
-        type: 'identifier',
-        value: 't'
+        //type: 'typeof',
+        //expression: {
+        //  type: "dereference",
+        //  expression: {
+            type: 'identifier',
+            value: 't'
+        //  }
+        //}
       }]
     }]
-  });
+  }));
 }
 
 main();
